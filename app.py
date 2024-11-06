f24077376a5be5d33fa9bf4a1b7499d59caf69be
vamshi0903 <vamshidhar127@gmail.com>
2024-11-06 17:18:13 -0600

    db change to tmp

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/site.db?timeout=30'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/site.db?timeout=30'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'your_secret_key'  # Change this to a real secret key in production sqlite:///site.db?timeout=30
@@ -699,8 +699,9 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        add_categories()
+    if not os.path.exists('/tmp/site.db'):
+        with app.app_context():
+            db.create_all()
+            add_categories() 
     
     app.run(debug=True)
